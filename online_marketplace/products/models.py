d7c1a464f7624617ba86b026c6660697b36a0ab2
Martin <marto.1003@gmail.com>
2023-07-31 22:53:28 +0300

    Removed subcategory model from products

--- online_marketplace/products/models.py
+++ online_marketplace/products/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     upload_date = models.DateTimeField(default=timezone.now)
@@ -35,7 +35,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     # subcategory = models.ForeignKey(SubCategory, related_name='products', null=True, blank=True, on_delete=models.SET_NULL)
     condition = models.CharField(max_length=20, choices=CONDITION_CHOICES, default='New')
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to='static/products/', null=True, blank=True)
 
     def __str__(self):
         return self.name
