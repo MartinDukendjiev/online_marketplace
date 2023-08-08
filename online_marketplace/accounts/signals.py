9081e4230cb19d076e371d1dfa791746decdb36a
Martin <marto.1003@gmail.com>
2023-08-08 12:07:25 +0300

    Editing templates

--- online_marketplace/accounts/signals.py
+++ online_marketplace/accounts/signals.py
@@ -1,62 +1,3 @@
-# from django.contrib.auth import get_user_model
-# from django.contrib.auth.models import Group, Permission
-# from django.contrib.contenttypes.models import ContentType
-# from django.core.mail import send_mail
-# from django.db.models.signals import post_save, post_migrate
-# from django.dispatch import receiver
-# from django.template.loader import render_to_string
-# from django.utils.html import strip_tags
-#
-# from online_marketplace import settings
-#
-# UserModel = get_user_model()
-#
-#
-# def sand_successful_registration_email(user):
-#     html_message = render_to_string(
-#         'emails/email-greeting.html',
-#         {'user': user}
-#     )
-#
-#     plain_massage = strip_tags(html_message)  # removes tags and shows only the text of the HTML
-#
-#     send_mail(
-#         subject='Successful Registration',
-#         message=plain_massage,
-#         html_message=html_message,
-#         from_email=settings.EMAIL_HOST_USER,
-#         recipient_list=(user.email,)
-#     )
-#
-#
-# @receiver(post_save, sender=UserModel)
-# def user_created(instance, created, **kwargs):
-#     if created:
-#         sand_successful_registration_email(instance)
-#
-#
-# @receiver(post_migrate)
-# def add_user_groups(sender, **kwargs):
-#     superuser_permissions = Permission.objects.all()  # have all permissions
-#
-#     content_type_product = ContentType.objects.get(app_label='products', model='product')
-#     content_type_category = ContentType.objects.get(app_label='products', model='category')
-#     content_type_subcategory = ContentType.objects.get(app_label='products', model='subcategory')
-#
-#     content_types = [content_type_product, content_type_category, content_type_subcategory]
-#     staff_permissions = Permission.objects.filter(content_type__in=content_types)
-#
-#     superuser_group, created = Group.objects.get_or_create(name='Superuser')
-#     if created:
-#         superuser_group.permissions.set(superuser_permissions)
-#
-#     staff_group, created = Group.objects.get_or_create(name='Staff')
-#     if created:
-#         staff_group.permissions.set(staff_permissions)
-
-
-
-from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
@@ -77,7 +18,7 @@ def send_successful_registration_email(user):
         {'user': user}
     )
 
-    plain_message = strip_tags(html_message)  # removes tags and shows only the text of the HTML
+    plain_message = strip_tags(html_message)
 
     send_mail(
         subject='Successful Registration',
@@ -96,9 +37,9 @@ def user_created(instance, created, **kwargs):
 
 @receiver(post_migrate)
 def add_user_groups(sender, **kwargs):
-    if kwargs.get('app') and kwargs.get('app').name == 'your_app_name':  # Проверка за приложението
-        if kwargs.get('created_models'):  # Проверка за създадени модели
-            superuser_permissions = Permission.objects.all()  # have all permissions
+    if kwargs.get('app') and kwargs.get('app').name == 'your_app_name':
+        if kwargs.get('created_models'):
+            superuser_permissions = Permission.objects.all()
 
             content_type_product = ContentType.objects.get(app_label='products', model='product')
             content_type_category = ContentType.objects.get(app_label='products', model='category')
